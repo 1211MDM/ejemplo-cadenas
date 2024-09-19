39feac0faee4b358c2c1c2b66bf402cc546ce559
María del Mar Papamija <maria27papamija@hotmail.com>
2024-09-19 17:26:56 -0500

    Se agrego un nuevo archivo

---
+++ cadenaProject/taller.py
@@ -0,0 +1,11 @@
+#1.
+name = "Yenifer"
+age = 30
+favoriteFood = "Pasta con salsa boloñesa"
+print(f"hola, me llamo {name}, tengo {age} año y me gusta la {favoriteFood}")
+
+#2.
+fullname = input('Ingresa su nombre completo:')
+print(fullname.count())
+
+#3.
